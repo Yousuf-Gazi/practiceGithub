bd9b01bb3d61b493b4246a686d38f7873138ae7b
hazratali-uydevelopers <hazratali.uydevelopers@gmail.com>
2023-03-01 23:41:25 +0600

    added substraction function

--- practice.cpp
+++ practice.cpp
@@ -5,7 +5,12 @@ int add(int a, int b) {
     return a + b;
 }
 
+int sub(int a, int b) {
+    return a - b;
+}
+
 int main() {
     cout << add(10, 20) << endl;
+    cout << add(100, 20) << endl;
     return 0;
 }
